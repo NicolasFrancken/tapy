0bf402dfb5c908732a2cbf72c71944ad76a213e3
Jorge Assaf <jorgeassaf160@gmail.com>
2024-02-14 12:04:57 -0600

    FE/Refactor main header component layout

--- app/components/ui/layout/main-header.tsx
+++ app/components/ui/layout/main-header.tsx
@@ -2,8 +2,10 @@ import { Logo } from "../icons";
 
 export const MainHeader = () => {
   return (
-    <header className="container sticky top-0 flex justify-center py-14 lg:justify-start">
-      <Logo />
+    <header className=" sticky top-0 z-40 bg-background/70 py-14 backdrop-blur-sm ">
+      <div className="container flex w-full justify-center lg:justify-start ">
+        <Logo />
+      </div>
     </header>
   );
 };
