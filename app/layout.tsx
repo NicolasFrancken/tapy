e7449ffa9f68f85a39632b1642785c579285e9ea
Jorge Assaf <jorgeassaf160@gmail.com>
2024-02-14 11:17:30 -0600

    FE/Add layout components for pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { cn } from "./client/libs/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn("antialiased", outfit.className)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
