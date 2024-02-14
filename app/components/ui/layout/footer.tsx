d87d0f67e903b6aba7f99333e67d1bebe2831bfd
Jorge Assaf <jorgeassaf160@gmail.com>
2024-02-14 11:18:20 -0600

    FE/Add footer component with social media buttons

---
+++ app/components/ui/layout/footer.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/app/client/components/ui/button";
+import { Instagram } from "lucide-react";
+import Link from "next/link";
+import { Tiktok } from "../icons";
+
+function Footer() {
+  return (
+    <footer className="container flex items-center justify-between border-t py-8">
+      <div className="absolute inset-x-0 bottom-0 h-16 w-full bg-primary  blur-[120px]" />
+      <p>@Copyrigth {new Date().getFullYear()}</p>
+      <div className="flex gap-4">
+        <Button
+          asChild
+          variant="outline"
+          size="icon"
+          className="rounded-xl bg-white hover:bg-background/20"
+          aria-label="Instagram"
+        >
+          <Link href="/">
+            <Instagram />
+            <span className="sr-only">Instagram</span>
+          </Link>
+        </Button>
+        <Button
+          asChild
+          variant="outline"
+          size="icon"
+          className="rounded-xl bg-white hover:bg-background/20"
+          aria-label="Tiktok"
+        >
+          <Link href="/">
+            <Tiktok />
+            <span className="sr-only">Tiktok</span>
+          </Link>
+        </Button>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
