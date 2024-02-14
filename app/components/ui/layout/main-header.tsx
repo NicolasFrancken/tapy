99d4346702a2546fb0121dc66ded91cb107cbd93
Jorge Assaf <jorgeassaf160@gmail.com>
2024-02-14 13:49:48 -0600

    FE/Update MainHeader backdrop blur size

--- app/components/ui/layout/main-header.tsx
+++ app/components/ui/layout/main-header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../icons";
 
 export const MainHeader = () => {
   return (
-    <header className=" sticky top-0 z-40 bg-background/70 py-14 backdrop-blur-sm ">
+    <header className=" sticky top-0 z-40 bg-background/70 py-14 backdrop-blur-lg ">
       <div className="container flex w-full justify-center lg:justify-start ">
         <Logo />
       </div>
