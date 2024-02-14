e604ed35f5e21841cf4ea8b0184fda2f1c890f31
Jorge Assaf <jorgeassaf160@gmail.com>
2024-02-14 11:18:55 -0600

    FE/Add MainHeader component

---
+++ app/components/ui/layout/main-header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "../icons";
+
+export const MainHeader = () => {
+  return (
+    <header className="container sticky top-0 flex justify-center py-14 lg:justify-start">
+      <Logo />
+    </header>
+  );
+};
